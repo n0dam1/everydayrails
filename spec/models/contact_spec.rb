9ac705447707bb281adc9ec8ccd1ac2ce587810c
n0dam1 <minoru.noda.0527@gmail.com>
2017-12-10 22:47:29 +0900

    Modify factories directory

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -12,30 +12,32 @@ describe Contact do
       firstname: 'Aaron',
       lastname: 'Sumner',
       email: 'tester@example.com')
-      contact.valid?
+    contact.valid?
     expect(contact).to be_valid
   end
 
   # 名がなければ無効な状態であること
   it 'is invalid without a firstname' do
-    contact = Contact.new(firstname: nil)
+    contact = FactoryGirl.build(:contact, firstname: nil)
     contact.valid?
     expect(contact.errors[:firstname]).to include("can't be blank")
   end
 
   # 姓がなければ無効な状態であること
   it 'is invalid without a lastname' do
-    contact = Contact.new(lastname: nil)
+    contact = FactoryGirl.build(:contact, lastname: nil)
     contact.valid?
     expect(contact.errors[:lastname]).to include("can't be blank")
   end
 
-  # メールアドレスが重複する場合は無効な状態である
+  # メールアドレスがなければ無効な状態であること
   it 'is invalid without an email address' do
-    contact = Contact.new(lastname: nil)
+    contact = FactoryGirl.build(:contact, email: nil)
     contact.valid?
-    expect(contact.errors[:lastname]).to include("can't be blank")
+    expect(contact.errors[:email]).to include("can't be blank")
   end
+
+  # メールアドレスが重複する場合は無効な状態である
   it 'is invalid with a duplicate email address' do
     Contact.create(
       firstname: 'Joe', lastname: 'Tester',
@@ -51,10 +53,7 @@ describe Contact do
 
   # 連絡先のフルネームを文字列として返すこと
   it "returns a contact's full name as a string" do
-    contact = Contact.new(
-      firstname: 'John', lastname: 'Doe',
-      email: 'johndoe@example.com'
-    )
+    contact = FactoryGirl.build(:contact, firstname: 'John', lastname: 'Doe')
     expect(contact.name).to eq 'John Doe'
   end
 
