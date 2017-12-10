8b73824f7b6ddc7c83abd67685bfa838187e0861
n0dam1 <minoru.noda.0527@gmail.com>
2017-12-10 23:16:44 +0900

    Modify factories directory

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -3,58 +3,53 @@ require 'rails_helper'
 describe Contact do
   # 有効なファクトリを持つこと
   it "has a valid factory" do
-    expect(FactoryGirl.build(:contact)).to be_valid
+    expect(build(:contact)).to be_valid
+  end
+  # 3つの電話番号を持つこと
+  it "has three phone numbers" do
+    expect(create(:contact).phones.count).to eq 3
   end
 
   # 姓と名があれば有効な状態であること
   it 'is valid with a firstname, lastname and email' do
-    contact = Contact.new(
-      firstname: 'Aaron',
-      lastname: 'Sumner',
-      email: 'tester@example.com')
+    contact = build(:contact)
     contact.valid?
     expect(contact).to be_valid
   end
 
   # 名がなければ無効な状態であること
   it 'is invalid without a firstname' do
-    contact = FactoryGirl.build(:contact, firstname: nil)
+    contact = build(:contact, firstname: nil)
     contact.valid?
     expect(contact.errors[:firstname]).to include("can't be blank")
   end
 
   # 姓がなければ無効な状態であること
   it 'is invalid without a lastname' do
-    contact = FactoryGirl.build(:contact, lastname: nil)
+    contact = build(:contact, lastname: nil)
     contact.valid?
     expect(contact.errors[:lastname]).to include("can't be blank")
   end
 
   # メールアドレスがなければ無効な状態であること
   it 'is invalid without an email address' do
-    contact = FactoryGirl.build(:contact, email: nil)
+    contact = build(:contact, email: nil)
     contact.valid?
     expect(contact.errors[:email]).to include("can't be blank")
   end
 
-  # メールアドレスが重複する場合は無効な状態である
+  # 重複したメールアドレスなら無効な状態であること
   it 'is invalid with a duplicate email address' do
-    Contact.create(
-      firstname: 'Joe', lastname: 'Tester',
-      email: 'tester@example.com'
-    )
-    contact = Contact.new(
-      firstname: 'Jane', lastname: 'Tester',
-      email: 'tester@example.com'
-    )
+    create(:contact, email: 'aaron@example.com')
+    contact = build(:contact, email: 'aaron@example.com')
     contact.valid?
     expect(contact.errors[:email]).to include("has already been taken")
   end
 
   # 連絡先のフルネームを文字列として返すこと
   it "returns a contact's full name as a string" do
-    contact = FactoryGirl.build(:contact, firstname: 'John', lastname: 'Doe')
-    expect(contact.name).to eq 'John Doe'
+    contact = build(:contact, firstname: 'Jane', lastname: 'Smith')
+    expect(contact.name).to eq 'Jane Smith'
   end
 
   # 文字で姓をフィルタする
